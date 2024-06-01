00ae3f161dc9cafa164df47439a934856b3633bc
Lakshman Kumar <109958931+LakshmanKumar01@users.noreply.github.com>
2024-06-01 21:47:30 +0530

    Create Weather.js

---
+++ Weather.js
@@ -0,0 +1,14 @@
+// The value of Kelvin is 293 degrees
+const kelvin=293;
+// Let's Convert Celsius to Kelvin 
+let celsius= kelvin - 273;
+// Calculating Fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+// Getting fahrenheit value
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+// Converting Celsius to Newton
+let Newton = celsius * (33/100);
+// Newton's Value
+Newton = Math.floor(Newton);
+console.log(`The temperature is ${Newton} degrees`);
