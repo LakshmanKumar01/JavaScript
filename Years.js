00a4af4dd400773df34d6e21df64667f21fd88b6
Lakshman Kumar <109958931+LakshmanKumar01@users.noreply.github.com>
2024-06-01 21:48:51 +0530

    Create Years.js

---
+++ Years.js
@@ -0,0 +1,12 @@
+// Years of Humanbeings
+let myAge = 30;
+let earlyYears = 2;
+earlyYears = myAge * earlyYears;
+// In EarlyYears my Age is
+let laterYears = myAge - 2;
+laterYears = laterYears * 4;
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+var myName = 'Lakshman'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears}`);
