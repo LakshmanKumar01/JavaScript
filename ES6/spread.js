fb0523451144c806c0429518b65dc1a5a872732e
LakshmanKumar01 <gollalakshmankumar2003@gmail.com>
2024-08-27 22:00:04 +0530

    ES6 Version sample progrmas

---
+++ ES6/spread.js
@@ -0,0 +1,13 @@
+// let arr = [1,2,3,4,5];
+// let arr2 = ["sai","kumar","golla"];
+// let arr3 = [true,false];
+// let arr4  = [...arr,...arr2,...arr3];
+// console.log(arr4);
+
+let n = 10;
+if(n%5==0){
+  console.log("div by 5");
+}
+else if(n%2==0){
+  console.log("div by 2");
+}
